c8194a65a9a77ef12ad01fe00abc93b30848bdaf
Ponnaganti Sravani <shaliniponnaganti@gmail.com>
2026-08-01 18:07:08 +0530

    updated codes

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -8,7 +8,8 @@ if (!env.databaseUrl) {
 }
 
 export const pool = new Pool({
-  connectionString: env.databaseUrl
+  connectionString: env.databaseUrl,
+  ssl: { rejectUnauthorized: false }
 });
 
 export async function verifyDatabaseConnection() {
